98ebda739db926d5ed7b11094eb187e3bbe20a76
HRicsi15 <rihard.horvath@stud.uni-corvinus.hu>
2021-09-26 16:49:31 +0200

    Add Resources

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -15,6 +15,10 @@ namespace UserMaintenance
         public Form1()
         {
             InitializeComponent();
+
+            lblLastName.Text = Resource1.LastName; //label1
+            lblFirstName.Text = Resource1.FirstName;
+            btnAdd.Text = Resource1.Add;
         }
     }
 }
