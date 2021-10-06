146c98879f45977d5ed85880e7f71a245a5bf72c
HRicsi15 <rihard.horvath@stud.uni-corvinus.hu>
2021-10-06 13:33:48 +0200

    Into memory

--- Excel generalasa w04/Excel generalasa w04/Form1.cs
+++ Excel generalasa w04/Excel generalasa w04/Form1.cs
@@ -12,9 +12,17 @@ namespace Excel_generalasa_w04
 {
     public partial class Form1 : Form
     {
+        RealEstateEntities context = new RealEstateEntities();
+        List<Flat> Flats;
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            List<Flat> Flats = context.Flats.ToList();//memoryba másoljuk a szerverről
         }
     }
 }
