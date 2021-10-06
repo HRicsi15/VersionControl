96d02fd37e579407d0a43ca24dca2f20e8dc2092
HRicsi15 <rihard.horvath@stud.uni-corvinus.hu>
2021-10-06 13:52:20 +0200

    Create variables

--- Excel generalasa w04/Excel generalasa w04/Form1.cs
+++ Excel generalasa w04/Excel generalasa w04/Form1.cs
@@ -17,18 +17,26 @@ namespace Excel_generalasa_w04
         RealEstateEntities context = new RealEstateEntities();
         List<Flat> Flats;
 
-        
+        Excel.Application xlApp; // A Microsoft Excel alkalmazás
+        Excel.Workbook xlWB; // A létrehozott munkafüzet
+        Excel.Worksheet xlSheet; // Munkalap a munkafüzeten belül
+
         public Form1()
         {
             InitializeComponent();
             LoadData();
 
-          
+            CreateExcel();
         }
 
         private void LoadData()
         {
             List<Flat> Flats = context.Flats.ToList();//memoryba másoljuk a szerverről
+        }
+
+        private void CreateExcel()
+        {
+
         }
     }
 }
