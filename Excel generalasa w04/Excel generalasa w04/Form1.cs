3dd921105b345b67e7f8b1f9653ae8a6fb162ea3
HRicsi15 <rihard.horvath@stud.uni-corvinus.hu>
2021-10-07 19:28:54 +0200

    FormatTable

--- Excel generalasa w04/Excel generalasa w04/Form1.cs
+++ Excel generalasa w04/Excel generalasa w04/Form1.cs
@@ -133,7 +133,7 @@ namespace Excel_generalasa_w04
 
 
 
-           /* Excel.Range headerRange = xlSheet.get_Range(GetCell(1, 1), GetCell(1, headers.Length));
+            Excel.Range headerRange = xlSheet.get_Range(GetCell(1, 1), GetCell(1, headers.Length));
             headerRange.Font.Bold = true;
             headerRange.VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
             headerRange.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
@@ -141,7 +141,18 @@ namespace Excel_generalasa_w04
             headerRange.RowHeight = 40;
             headerRange.Interior.Color = Color.LightBlue;
             headerRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
-                */
+
+
+            Excel.Range tableRange = xlSheet.get_Range(GetCell(2, 1), GetCell(1 + values.GetLength(0), values.GetLength(1)));
+            tableRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
+
+            Excel.Range firstColumn = xlSheet.get_Range(GetCell(2, 1), GetCell(1 + values.GetLength(0), 1));
+            firstColumn.Font.Bold = true;
+            firstColumn.Interior.Color = Color.LightYellow;
+
+            Excel.Range lastColumn = xlSheet.get_Range(GetCell(2, values.GetLength(1)), GetCell(1 + values.GetLength(0), values.GetLength(1)));
+            firstColumn.NumberFormat = "###,###,00";
+            firstColumn.Interior.Color = Color.LightGreen;
         }
     }
 
