c986c460a3e66e5e43967beff90fc7b6cef3186f
HRicsi15 <rihard.horvath@stud.uni-corvinus.hu>
2021-10-19 19:52:36 +0200

    Load Data

--- w5_var/w5_var/Form1.cs
+++ w5_var/w5_var/Form1.cs
@@ -14,15 +14,15 @@ namespace w5_var
     public partial class Form1 : Form
     {
         List<Tick> ticks;
-       // PortfolioEntities context = new PortfolioEntities();
+        PortfolioEntities context = new PortfolioEntities();
 
         public Form1()
         {
             InitializeComponent();
             /*context.Ticks.Load();
             dataGridView1.DataSource = context.Ticks.Local;*/ //talán lehet így is, lehet ideiglenes 
-          //  ticks = context.Ticks.ToList();
-           // dataGridView1.DataSource = ticks;
+            ticks = context.Ticks.ToList();
+            dataGridView1.DataSource = ticks;
         }
     }
 }
