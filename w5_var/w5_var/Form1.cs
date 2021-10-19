88ba993012afd4a84d8b91ccfdff349d82590542
HRicsi15 <rihard.horvath@stud.uni-corvinus.hu>
2021-10-19 19:52:11 +0200

    Create ORM

---
+++ w5_var/w5_var/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace w5_var
+{
+    public partial class Form1 : Form
+    {
+        List<Tick> ticks;
+       // PortfolioEntities context = new PortfolioEntities();
+
+        public Form1()
+        {
+            InitializeComponent();
+            /*context.Ticks.Load();
+            dataGridView1.DataSource = context.Ticks.Local;*/ //talán lehet így is, lehet ideiglenes 
+          //  ticks = context.Ticks.ToList();
+           // dataGridView1.DataSource = ticks;
+        }
+    }
+}
