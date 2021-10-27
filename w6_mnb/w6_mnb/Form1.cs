ce20f4d6415d9145bfab3c2c5a9f38284962b5d9
HRicsi15 <rihard.horvath@stud.uni-corvinus.hu>
2021-10-27 10:58:50 +0200

    Webszolgáltatás hívása

--- w6_mnb/w6_mnb/Form1.cs
+++ w6_mnb/w6_mnb/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using w6_mnb.MnbServiceReference;
 
 namespace w6_mnb
 {
@@ -15,6 +16,24 @@ namespace w6_mnb
         public Form1()
         {
             InitializeComponent();
+
+            GetExchangeRates();
+        }
+
+        private void GetExchangeRates()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);//bemenetis függvény
+
+            var result = response.GetExchangeRatesResult;//az eredmény tulajdonsága
         }
     }
 }
