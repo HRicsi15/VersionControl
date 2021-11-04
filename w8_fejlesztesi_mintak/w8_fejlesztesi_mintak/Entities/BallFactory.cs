982893554812221edb81fd80b0f02a2299b14ebe
HRicsi15 <rihard.horvath@stud.uni-corvinus.hu>
2021-11-04 18:16:57 +0100

    Labdagyár létrehozása

---
+++ w8_fejlesztesi_mintak/w8_fejlesztesi_mintak/Entities/BallFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace w8_fejlesztesi_mintak.Entities
+{
+    class BallFactory
+    {
+        public Ball CreateNew() //Ball viszatérési érték
+        {
+            return new Ball(); //fgv-ben léterhozunk 1Ball példányt és visszaadjuk az értékét
+        }
+    }
+}
