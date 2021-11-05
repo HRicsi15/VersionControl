5b8f9b42b721175279e55c9e8e87ef7bb15dbb76
HRicsi15 <rihard.horvath@stud.uni-corvinus.hu>
2021-11-05 18:08:23 +0100

    Absztrakció felhasználása a labdában

--- w8_fejlesztesi_mintak/w8_fejlesztesi_mintak/Entities/Ball.cs
+++ w8_fejlesztesi_mintak/w8_fejlesztesi_mintak/Entities/Ball.cs
@@ -5,32 +5,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using w8_fejlesztesi_mintak.Abstractions;
 
 namespace w8_fejlesztesi_mintak.Entities
 {
-    public class Ball : Label
+    public class Ball : Toy
     {
-        public Ball()
-        {
-            AutoSize = false;
-            Width = 50;
-            Height = 50; 
-            Paint += Ball_Paint;
-        }
+               /* public Ball() //ez már nem kell, toyban van. 
+                {
+                    AutoSize = false;
+                    Width = 50;
+                    Height = 50; 
+                    Paint += Ball_Paint;
+                }
 
-        private void Ball_Paint(object sender, PaintEventArgs e)
-        {
-            DrawImage(e.Graphics);
-        }
+                private void Ball_Paint(object sender, PaintEventArgs e)
+                {
+                    DrawImage(e.Graphics);
+                }*/
 
-        protected void DrawImage(Graphics g)
+        protected override void DrawImage(Graphics g)//meglévő toy fgv-t módosítjuk
         {
             g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height); //kék kör kirajzolása
         }
-
-        public void MoveBall()
-        {
-            Left += 1;//eggyel növeljük 
-        }
+                /*
+                public void MoveBall()
+                {
+                    Left +=1;//eggyel növeljük 
+                }*/
     }
 }
