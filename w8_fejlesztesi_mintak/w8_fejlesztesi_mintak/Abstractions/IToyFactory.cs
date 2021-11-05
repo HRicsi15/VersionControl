369ba133b9a34fcd3e5a641e7e0dfbd144873967
HRicsi15 <rihard.horvath@stud.uni-corvinus.hu>
2021-11-05 18:32:37 +0100

    Gyár absztrakció kezelése

---
+++ w8_fejlesztesi_mintak/w8_fejlesztesi_mintak/Abstractions/IToyFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace w8_fejlesztesi_mintak.Abstractions
+{
+   public interface IToyFactory //publicnak kell lennie!
+    {
+        Toy CreateNew(); //fgv, visszateresi értéke Toy típusú. Ezt is visszük tovább
+    }
+}
