05e60803d19f0325437ab68c9f68145327629ac4
HRicsi15 <rihard.horvath@stud.uni-corvinus.hu>
2021-11-21 20:03:02 +0100

    Jelszó validálás függvény

--- w9_unit/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ w9_unit/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using UnitTestExample.Controllers;
 
@@ -33,14 +34,36 @@ namespace UnitTestExample.Test
           TestCase("abcdABCD", false)]//nincs szám
         public void TestValidatePassword(string password, bool expectedResult)
         {
+            /*
             // Arrange
             var accountController = new AccountController();
             // Act
             var actualResult = accountController.ValidatePassword(password);
             // Assert
-            Assert.AreEqual(expectedResult, actualResult);
+            Assert.AreEqual(expectedResult, actualResult);*/
 
+            Regex regex = new Regex("(^[a-zA-Z0-9]{8,}$)");
+            if (regex.IsMatch(password))
+            {
+                Regex regex2 = new Regex("(^[a-z]+$)");
+                if (regex2.IsMatch(password))
+                {
+                    Regex regex3 = new Regex("(^[A-Z]+$)");
+                    if (regex3.IsMatch(password))
+                    {
+                        Regex regex4 = new Regex("(^[0-9]+$)");
+                        if (regex4.IsMatch(password))
+                        {
+                            expectedResult = true;
+                        }
+                        else { expectedResult = false; }
+                    }
+                    else { expectedResult = false; }
+                }
+                else { expectedResult = false; }
+                   
+            }
+            else { expectedResult = false; }
         }
-
     }
 }
